d626b892804bd0a9052a8b8b13be83a6d8086c4a
BD24K <64867736+JosueFlores777@users.noreply.github.com>
2024-04-17 08:24:26 -0600

    Update endpoints.js ruta de back

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,5 +1,5 @@
 export default {
   auth: 'http://40.87.120.90:8081/api/',
-  backend: 'http://localhost:8081/api/',
+  backend: 'http://40.87.120.90:8082/api/',
   backendArchivos: 'http://40.87.120.90:8083/api/',
 };
