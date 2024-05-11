a72cb8332561d89479b21dd8aee7cab990066d72
Cristian Palma <cristianpalma1703@gmail.com>
2024-05-11 16:57:18 -0600

    change endpoint

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -3,6 +3,6 @@ export default {
   backend: 'http://localhost:8081/api/',
   backendArchivos: 'http://localhost:52740/api/',*/
   auth: 'http://40.87.120.90:8081/api/',
-  backend: 'http://localhost:8081/api/',
+  backend: 'http://40.87.120.90:8082/api/',
   backendArchivos: 'http://40.87.120.90:8083/api/',
 };
